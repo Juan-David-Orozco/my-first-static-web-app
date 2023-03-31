5e06132e234adeb0f75a0f6918db752fdda24e3a
Juan-David-Orozco <ing.ele2018@gmail.com>
2023-03-31 16:12:09 -0500

    add API

--- src/App.js
+++ src/App.js
@@ -1,8 +1,16 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello {value}</div>;
+  const [data, setData] = useState('');
+
+  useEffect(() => {
+    (async function () {
+      const { text } = await( await fetch(`/api/HttpTrigger`)).json();
+      setData(text);
+    })();
+  });
+
+  return <div>{data}</div>;
 }
 
 export default App;
